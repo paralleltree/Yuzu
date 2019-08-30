9870dcc49ed52beeff496e2ff1eb6e2a3f9f9875
Paralleltree <paralleltree@outlook.com>
2019-08-30 11:35:21 +0900

    IOperationとOperationManagerを追加

---
+++ Yuzu/UI/Operations/IOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuzu.UI.Operations
+{
+    /// <summary>
+    /// ユーザの操作を表すインターフェースです。
+    /// </summary>
+    internal interface IOperation
+    {
+        /// <summary>
+        /// この操作の説明を取得します。
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// 操作を元に戻します。
+        /// </summary>
+        void Undo();
+
+        /// <summary>
+        /// 操作をやり直します。
+        /// </summary>
+        void Redo();
+    }
+}
