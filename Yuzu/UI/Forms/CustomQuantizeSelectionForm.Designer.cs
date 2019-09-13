1d7a74576b74a60e3e2485c6f2e45cdb965eaa9f
Paralleltree <paralleltree@outlook.com>
2019-09-13 11:50:08 +0900

    カスタム音符指定フォーム追加

---
+++ Yuzu/UI/Forms/CustomQuantizeSelectionForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Yuzu.UI.Forms
+{
+    partial class CustomQuantizeSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lengthBox = new System.Windows.Forms.ComboBox();
+            this.divisionBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.divisionBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "分音符を";
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.FormattingEnabled = true;
+            this.lengthBox.Location = new System.Drawing.Point(39, 26);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.Size = new System.Drawing.Size(59, 20);
+            this.lengthBox.TabIndex = 1;
+            // 
+            // divisionBox
+            // 
+            this.divisionBox.Location = new System.Drawing.Point(160, 27);
+            this.divisionBox.Name = "divisionBox";
+            this.divisionBox.Size = new System.Drawing.Size(65, 19);
+            this.divisionBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "分割する";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(149, 68);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(230, 68);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // CustomQuantizeSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 103);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.divisionBox);
+            this.Controls.Add(this.lengthBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CustomQuantizeSelectionForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "カスタム音符指定";
+            ((System.ComponentModel.ISupportInitialize)(this.divisionBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox lengthBox;
+        private System.Windows.Forms.NumericUpDown divisionBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Yuzu/UI/Forms/CustomQuantizeSelectionForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yuzu.UI.Forms
+{
+    public partial class CustomQuantizeSelectionForm : Form
+    {
+        private int BarTick { get; }
+
+        public double QuantizeTick => Math.Max(BarTick / Math.Pow(2, lengthBox.SelectedIndex) / (int)divisionBox.Value, 1);
+
+        public CustomQuantizeSelectionForm(int barTick)
+        {
+            InitializeComponent();
+
+            AcceptButton = buttonOK;
+            CancelButton = buttonCancel;
+            buttonOK.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            BarTick = barTick;
+
+            lengthBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            lengthBox.Items.AddRange(Enumerable.Range(0, 7).Select(p => ((int)Math.Pow(2, p)).ToString()).ToArray());
+            lengthBox.SelectedIndex = 2;
+
+            divisionBox.Minimum = 1;
+            divisionBox.Maximum = 30;
+            divisionBox.Value = 1;
+        }
+    }
+}
